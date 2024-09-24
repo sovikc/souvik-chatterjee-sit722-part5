4471a086fe33a7b09f6a4fd52c73d140e7f94d4f
unknown <sovik.c.28@gmail.com>
2024-09-24 16:05:40 +1000

    added push code

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,1 +1,10 @@
 # Your Solution
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+: "$REGISTRY_UN"
+: "$REGISTRY_PW"
+
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+docker push $CONTAINER_REGISTRY/book-catalog:$VERSION
+docker push $CONTAINER_REGISTRY/inventory-management:$VERSION
