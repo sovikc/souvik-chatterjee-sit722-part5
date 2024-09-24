34abc6b847253c7d72a56b678a2bc5d6966e7d05
unknown <sovik.c.28@gmail.com>
2024-09-24 15:43:42 +1000

    changed scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -3,8 +3,10 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
+echo "started building"
 # build book-catalog
-docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile ./book_catalog
+docker build -t $CONTAINER_REGISTRY/book-catalog:$VERSION --file ./book_catalog/Dockerfile .
 
 # build inventory-management
-docker build -t $CONTAINER_REGISTRY/inventory-management:$VERSION --file ./inventory-management/Dockerfile ./inventory-management
+docker build -t $CONTAINER_REGISTRY/inventory-management:$VERSION --file ./inventory-management/Dockerfile .
+echo "after building"
